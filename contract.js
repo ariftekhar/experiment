89ce1c4ffb10b8fbf893b4facbec1f8ffac8afc4
ariftekhar <40751704+ariftekhar@users.noreply.github.com>
2020-04-12 20:35:55 +0600

    1

---
+++ contract.js
@@ -0,0 +1,4 @@
+// Draw some text to the screen:
+drawName('Contract me');
+// Animate the text!
+bounceBubbles();
---
+++ index.js
@@ -0,0 +1,4 @@
+// Draw some text to the screen:
+drawName('Welcome to my site');
+// Animate the text!
+bounceBubbles();
